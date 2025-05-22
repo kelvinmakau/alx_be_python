b522c6f3411a62ca1f578ba4035e4726277c5e7f
Kelvin Makau <kelvinmakau76@gmail.com>
2025-05-22 15:10:43 +0300

    First Commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+summ = number1 + number2
+diff = number1 - number2
+prod = number1 * number2
+
+print("Addition of 10 and 5 is " + str(summ))
+print("Subtraction of 10 and 5 is " + str(diff))
+print("Multiplication of 10 and 5 is " + str(prod))
