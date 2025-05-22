ab0d05d51c3693b1e20981d69dd698e15085a773
Kelvin Makau <kelvinmakau76@gmail.com>
2025-05-22 23:11:43 +0300

    Add future finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-savings = income - expenses
+income = input("Enter your monthly income: ")
+expenses = input("Enter your total monthly expenses: ")
+savings = float(income) - float(expenses)
 annual_savings = savings * 12 + (savings * 12 * 0.05)
 
 print("Your monthly savings are $" + str(savings))
