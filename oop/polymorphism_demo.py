6832d9ebafded7b46056f07ff2c7ebfac29a3886
Kelvin Makau <kelvinmakau76@gmail.com>
2025-06-21 15:40:34 +0300

    class and static methods

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,19 +4,21 @@ class Shape:
     def area(self):
        raise NotImplementedError("Subclasses must inherit the area method")
 
-class Rectangle(Shape):
+class Rectangle(Shape): # Inherits from shape class
     def __init__(self, length, width):
        super().__init__()
        self.length = length
        self.width = width
 
+    # override the area class to calculate area
     def area(self):
         return self.length * self.width
 
-class Circle(Shape):
+class Circle(Shape): # Inherits from shape class
     def __init__(self, radius):
         super().__init__()
         self.radius = radius
 
+    # override the area class to calculate area
     def area(self):
         return math.pi * (self.radius ** 2)
